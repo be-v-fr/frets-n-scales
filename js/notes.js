ecd8bd9527a1fe731da2df3cbb328d763293255e
Bengt Früchtenicht <bengt.fruechtenicht@gmx.de>
2023-12-01 13:18:58 +0100

    improved data structure

--- js/notes.js
+++ js/notes.js
@@ -1,1 +1,73 @@
-const NOTES = ['a', 'a#', 'b', 'c', 'c#', 'd', 'd#', 'e', 'f', 'g', 'g#'];
+const NOTES = [
+    {
+        default: 'a',
+        sharp: 'gx',
+        flat: 'bbb'
+    },
+
+    {
+        default: 'a#',
+        sharp: 'a#',
+        flat: 'bb'
+    },
+
+    {
+        default: 'b',
+        sharp: 'ax',
+        flat: 'cb'
+    },
+    
+    {
+        default: 'c',
+        sharp: 'b#',
+        flat: 'dbb'
+    },
+    
+    {
+        default: 'c#',
+        sharp: 'c#',
+        flat: 'db'
+    },
+
+    {
+        default: 'd',
+        sharp: 'cx',
+        flat: 'ebb'
+    },
+
+    {
+        default: 'd#',
+        sharp: 'd#',
+        flat: 'eb'
+    },
+
+    {
+        default: 'e',
+        sharp: 'dx',
+        flat: 'fb'
+    },
+
+    {
+        default: 'f',
+        sharp: 'e#',
+        flat: 'gbb'
+    },
+
+    {
+        default: 'f#',
+        sharp: 'ex',
+        flat: 'gb'
+    },
+
+    {
+        default: 'g',
+        sharp: 'fx',
+        flat: 'abb'
+    },
+
+    {
+        default: 'g#',
+        sharp: 'g#',
+        flat: 'ab'
+    },
+];
