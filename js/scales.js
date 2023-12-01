2fad6910731879d85803c30f5d0bf967ef8464b8
Bengt Früchtenicht <bengt.fruechtenicht@gmx.de>
2023-12-01 11:23:48 +0100

    added basic structure

---
+++ js/scales.js
@@ -0,0 +1,13 @@
+const REL_SCALES = {
+    'diatonic': [
+        {
+            'name': 'major',
+            'intervals': [0, 2, 4, 5, 7, 9, 11]
+        },
+
+        {
+            'name': 'minor',
+            'intervals': [0, 2, 3, 5, 7, 8, 10]
+        },
+    ]
+};
---
+++ js/tunings.js
@@ -0,0 +1,13 @@
+const REL_TUNINGS = {
+    'guitar': [
+        {
+            'name': 'standard',
+            'intervals': [0, 5, 10, 15, 19, 24]
+        },
+
+        {
+            'name': 'drop',
+            'intervals': [0, 7, 12, 17, 21, 26]
+        }
+    ]
+};
