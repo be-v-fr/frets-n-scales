adc2dfbed8199e2a839cebbaa846707a580fd867
Bengt Früchtenicht <bengt.fruechtenicht@gmx.de>
2023-12-01 12:02:05 +0100

    added more basics

--- js/scales.js
+++ js/scales.js
@@ -1,13 +1,13 @@
 const REL_SCALES = {
-    'diatonic': [
+    diatonic: [
         {
-            'name': 'major',
-            'intervals': [0, 2, 4, 5, 7, 9, 11]
+            name: 'major',
+            intervals: [0, 2, 4, 5, 7, 9, 11]
         },
 
         {
-            'name': 'minor',
-            'intervals': [0, 2, 3, 5, 7, 8, 10]
+            name: 'minor',
+            intervals: [0, 2, 3, 5, 7, 8, 10]
         },
     ]
 };
--- js/tunings.js
+++ js/tunings.js
@@ -1,13 +1,13 @@
 const REL_TUNINGS = {
-    'guitar': [
+    guitar: [
         {
-            'name': 'standard',
-            'intervals': [0, 5, 10, 15, 19, 24]
+            name: 'standard',
+            intervals: [0, 5, 10, 15, 19, 24]
         },
 
         {
-            'name': 'drop',
-            'intervals': [0, 7, 12, 17, 21, 26]
+            name: 'drop',
+            intervals: [0, 7, 12, 17, 21, 26]
         }
     ]
 };
